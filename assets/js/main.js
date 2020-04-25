a415ff98d7c0f5ae071923d5cdb5a6f3070357b3
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 17:21:44 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+var A = 'Sam is going to codingschool';
+
+console.log(A.slice(0,3))
+
+let s = A.slice(0,3);
+
+
+
+console.log(A.slice(4,6))
+
+let is = A.slice(4,6);
+
+console.log(A.slice(1,28))
+
+let langSatz=A.slice(1,28);
+
+console.log(A.slice(4,28))
+
+
+let langSatza=A.slice(4,28);
+
+console.log(A.slice(22,28))
+
+let school=A.slice(22,28);
+
+console.log(A.slice(0,3,))
+
+
+
+
+
+
+
+
+document.write("<h1>" + s + "</h1>"+ "</br>");
+document.write("<h1>" + is + "</h1>"+ "</br>");
+document.write("<h1>" + langSatz + "</h1>"+ "</br>");
+document.write("<h1>" + langSatza + "</h1>"+ "</br>");
+document.write("<h1>" + school + "</h1>"+ "</br>");
+document.write("<h1>" + s +" "+ is+" "+ school+""+"</h1>"+ "</br>");
